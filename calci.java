5fe5b6f8c6147c43598e1076e15dae5a0e684f18
Himanshu Patel <patelhimanshu609@gmail.com>
2026-01-13 15:32:47 +0530

    [Himanshu Patel] make subtraction

--- calci.java
+++ calci.java
@@ -2,4 +2,9 @@ public class calci {
     public static void main(String[] args) {
         System.out.println("Hello my friends");
     }
+
+    static int subtract(int a, int b){
+        return a-b;
+    }
 }
+
