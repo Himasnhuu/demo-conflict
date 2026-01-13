5785fe64f64f433bf8726178b3c883ce3c9c2445
Aryansh Chaudhary <abhichaudhary012002@gmail.com>
2026-01-13 15:34:47 +0530

    [Aryansh Chaudhary] refactor . addition

--- calci.java
+++ calci.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class calci {
     public static void main(String[] args) {
-        System.out.println("Hello my friends");
+        System.out.println("Basic Calculator");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int n1 = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int n2 = sc.nextInt();
+        int c = n1 + n2;
+        System.out.println(c);
+        sc.close();
     }
 }
